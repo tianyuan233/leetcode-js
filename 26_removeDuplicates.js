ae633217a2cbefd3349e95cbd78b8d43a5212c01
tianyuan233 <1406282384@qq.com>
2019-04-01 16:50:49 +0800

    removeDuplicates

---
+++ 26_removeDuplicates.js
@@ -0,0 +1,20 @@
+var removeDuplicates = function (nums) {
+    for (let index = 0; index < nums.length; index++) {
+        const element = nums[index];
+        const element2 = nums[index+1];
+        if (element == element2) {
+            nums.splice(index, 1);
+            index--;
+        }
+        console.log(nums);
+        
+    }
+
+    return nums
+
+
+};
+
+
+
+console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 4]))
