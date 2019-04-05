c40b6b9287fee920e844a0ae07101dbc53b009dd
tianyuan233 <1406282384@qq.com>
2019-04-05 11:29:39 +0800

    flipAndInvertImage

--- 832_flipAndInvertImage.js
+++ 832_flipAndInvertImage.js
@@ -1,29 +1,25 @@
-//solution 1
+// solution 1
 const flipAndInvertImage = function (A) {
   let temp = []
   for (let i = 0; i < A.length; i++) {
-    const element = A[i];
-    temp.push(element.reverse().map((x) => 1 ^ x));
+    const element = A[i]
+    temp.push(element.reverse().map((x) => 1 ^ x))
   }
-  return temp;
+  return temp
 }
 
-// console.log(
-//   flipAndInvertImage([
-//     [1, 1, 0],
-//     [1, 0, 1],
-//     [0, 0, 0]
-//   ])
-// );
+console.log(flipAndInvertImage([
+  [1, 1, 0],
+  [1, 0, 1],
+  [0, 0, 0]
+]))
 
-//solution 2
-const flipAndInvertImage2 = function (A) {
-  return A.map((arr) => arr.reverse()).map((arr) => arr.map((x) => 1^x));
+// solution 2
+let flipAndInvertImage2 = function (A) {
+  return A.map((arr) => arr.reverse()).map((arr) => arr.map((x) => 1 ^ x))
 }
-console.log(
-  flipAndInvertImage2([
-    [1, 1, 0],
-    [1, 0, 1],
-    [0, 0, 0]
-  ])
-);
+console.log(flipAndInvertImage2([
+  [1, 1, 0],
+  [1, 0, 1],
+  [0, 0, 0]
+]))
