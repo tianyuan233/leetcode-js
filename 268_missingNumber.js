b4d8957937be8c045fe4889971965158c25e9b70
tianyuan233 <1406282384@qq.com>
2019-04-19 12:07:35 +0800

    missingNumber

---
+++ 268_missingNumber.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+let missingNumber = function (nums) {
+  let sum = 0
+  let total = 0
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i]
+    total += i + 1
+  }
+  return total - sum
+}
+console.log(missingNumber([0, 1]))
+
